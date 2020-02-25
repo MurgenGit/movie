2464ebf82f7a5e39afbacb3aee3fdd1a92d0a5ee
Костя Галышев <dejerta@gmail.com>
2020-02-25 12:50:04 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  console.log(process.env.REACT_APP_SOME_KEY)
+  return (
+    <div className="App">
+        welcome to hooks
+    </div>
+  );
+}
+
+export default App;
